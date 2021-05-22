b101887451b40af4e594e93d944c5f52c795e7ef
jaster25 <js_328@naver.com>
2021-05-23 02:40:53 +0900

    Add insertion sort

--- 정렬/insertion_sort.c
+++ 정렬/insertion_sort.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 
 void insertion_sort (int* ar, int size) {
+  for (int i = 1; i < size ; i++)
+  {
+    int temp = ar[i];
+    int j = i - 1;
+    while ( j >= 0 && ar[j] >= temp ) {
+      ar[j + 1] = ar[j];
+      j--;
+    }
+    ar[j + 1] = temp;
+  }
 }
 
 int main() {
