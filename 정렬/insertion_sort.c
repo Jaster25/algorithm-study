dd982871a06c43c835fa6d99794e77ebc0583d3f
Jaster25 <52854045+Jaster25@users.noreply.github.com>
2021-05-23 15:04:57 +0900

    Add selection sort, Update code formatter

--- 정렬/insertion_sort.c
+++ 정렬/insertion_sort.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void insertion_sort (int* ar, int size) {
-  for (int i = 1; i < size ; i++)
+void insertion_sort(int *ar, int size)
+{
+  for (int i = 1; i < size; i++)
   {
     int temp = ar[i];
     int j = i - 1;
-    while ( j >= 0 && ar[j] >= temp ) {
+    while (j >= 0 && ar[j] >= temp)
+    {
       ar[j + 1] = ar[j];
       j--;
     }
@@ -13,16 +15,17 @@ void insertion_sort (int* ar, int size) {
   }
 }
 
-int main() {
-  int ar[10] = {10,3,7,1,4,9,2,5,8,6};
+int main()
+{
+  int ar[10] = {10, 3, 7, 1, 4, 9, 2, 5, 8, 6};
 
   printf("Before insertion sort\n");
-  for(int i=0 ; i< sizeof(ar) / sizeof(int) ; i++)
+  for (int i = 0; i < sizeof(ar) / sizeof(int); i++)
     printf("%d ", ar[i]);
 
   insertion_sort(ar, sizeof(ar) / sizeof(int));
 
   printf("\nAfter insertion sort\n");
-  for(int i=0 ; i< sizeof(ar) / sizeof(int) ; i++)
+  for (int i = 0; i < sizeof(ar) / sizeof(int); i++)
     printf("%d ", ar[i]);
 }
--- 정렬/selection_sort.c
+++ 정렬/selection_sort.c
@@ -1,18 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void selection_sort (int* ar, int size) {
+void selection_sort(int *ar, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    int minIndex = i;
+    for (int j = i; j < size; j++)
+      if (ar[j] < ar[minIndex])
+        minIndex = j;
+
+    int temp = ar[i];
+    ar[i] = ar[minIndex];
+    ar[minIndex] = temp;
+  }
 }
 
-int main() {
-  int ar[10] = {10,3,7,1,4,9,2,5,8,6};
+int main()
+{
+  int ar[10] = {10, 3, 7, 1, 4, 9, 2, 5, 8, 6};
 
   printf("Before selection sort\n");
-  for(int i=0 ; i< sizeof(ar) / sizeof(int) ; i++)
+  for (int i = 0; i < sizeof(ar) / sizeof(int); i++)
     printf("%d ", ar[i]);
 
   selection_sort(ar, sizeof(ar) / sizeof(int));
 
   printf("\nAfter selection sort\n");
-  for(int i=0 ; i< sizeof(ar) / sizeof(int) ; i++)
+  for (int i = 0; i < sizeof(ar) / sizeof(int); i++)
     printf("%d ", ar[i]);
 }
